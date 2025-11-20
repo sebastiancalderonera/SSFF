032ece860c517c14f3002d7c12a0f5bdb3dc748f
sebastiancalderonera <121590943+sebastiancalderonera@users.noreply.github.com>
2025-11-20 16:40:29 -0500

    Update popup.js
    Nuevo popup

--- popup.js
+++ popup.js
@@ -1,23 +1,30 @@
 (function () {
-    // URL de tu aplicación Fiori
-    var fioriUrl = "https://serviciossap.colombina.com/sap/bc/ui5_ui5/sap/zui5_vacaciones/index.html?sap-client=300";
+    function openPopup() {
+        var fioriUrl = "https://serviciossap.colombina.com/sap/bc/ui5_ui5/sap/zui5_vacaciones/index.html?sap-client=300";
 
-    // Tamaño deseado del popup
-    var width = 1100;
-    var height = 800;
+        var width = 1100;
+        var height = 800;
 
-    // Posición centrada en la pantalla
-    var left = (window.screen.width - width) / 2;
-    var top = (window.screen.height - height) / 2;
+        var left = (window.screen.width - width) / 2;
+        var top = (window.screen.height - height) / 2;
 
-    // Abrir popup con propiedades controladas
-    window.open(
-        fioriUrl,
-        "popupFioriVacaciones",
-        "toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes" +
-        ",width=" + width +
-        ",height=" + height +
-        ",top=" + top +
-        ",left=" + left
-    );
+        window.open(
+            fioriUrl,
+            "popupFioriVacaciones",
+            "popup=yes,toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes" +
+            ",width=" + width +
+            ",height=" + height +
+            ",top=" + top +
+            ",left=" + left
+        );
+    }
+
+    // Crear un botón dentro de la Card / página:
+    var button = document.createElement("button");
+    button.textContent = "Abrir Vacaciones";
+    button.style.padding = "10px 20px";
+    button.style.fontSize = "16px";
+    button.onclick = openPopup;
+
+    document.body.appendChild(button);
 })();
